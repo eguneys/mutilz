e99eae9f0731cc9df4cef75bb197d9bd5998fb87
Emre Guneyler <eguneys@hotmail.com>
2019-09-12 18:50:05 +0000

    readme source

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # mutilz
 Utility math functions for graphics programming like random choices, smoothstep round etc.
+
+_PI_ constant PI
+_TAU_ constant PI * 2.0
+
+_rad(degree)_ convert degree to radian
+
+
+_rand(min, max)_ random float between min, max
+
+_randInt(min, max)_ random integer between min, max
+
+_arand(values)_ pick a random item from an array
+
+_weightedChoice(weights)_ pick a random value out of given items based on given weights. 
+Input is in the form `[[item, weight], [item2, weight2]]`, where weight is between 0 and 1,
+ and all weights should add up to 1.
+
+_clamp(v, min, max)_ clamp v to min and max
+
+_usin(v)_ sin value in range 0 and 1
+
+_smoothstep(min, max, value)_ smoothstep value
+
+_round(v, d = 100)_ round value to `d` digits
+
+_map(n, start1, stop1, start2, stop2)_ map value `n` from range `start1/stop1` to `start2/stop2`
+
---
+++ util.js
@@ -0,0 +1,54 @@
+export const PI = Math.PI;
+export const TAU = PI * 2.0;
+export function rad(degree) {
+  return degree * Math.PI / 180;
+}
+export function rand(min, max) {
+  return Math.random() * (max - min) + min;
+}
+export function randInt(min, max) {
+  return Math.floor(rand(min, max));
+}
+export function arand(values) {
+  return values[randInt(0, values.length)];
+}
+export function clamp(v, min, max) {
+  return Math.min(Math.max(v, min), max);
+}
+export function usin(v) {
+  return (Math.sin(v) + 1.0) / 2.0;
+}
+export function smoothstep(min, max, value) {
+  var x = Math.max(0, Math.min(1, (value - min) / (max - min)));
+  return x * x * (3 - 2 * x);
+}
+;
+export function round(v, d = 100) {
+  return Math.round(v * d) / d;
+}
+export function map(n, start1, stop1, start2, stop2) {
+  return (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+}
+export function weightedChoice(weights) {
+  let r = rand(0, 1);
+
+  function step(weightSeen, remainings) {
+    let remaining = remainings[0];
+    let rest = remainings.slice(1);
+
+    if (remainings.length === 1) {
+      return remaining[0];
+    }
+
+    let newWeight = remaining[1],
+        endBound = weightSeen + newWeight;
+
+    if (r > weightSeen && r < endBound) {
+      return remaining[0];
+    }
+
+    return step(weightSeen + remaining[1], rest);
+  }
+
+  return step(0, weights);
+}
